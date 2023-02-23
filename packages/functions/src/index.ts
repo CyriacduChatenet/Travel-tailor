23effe258fefc9e9a5cbf13143eaec5bea5ffc31
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-02-23 16:36:52 +0100

    [FIX]: fix dependancies from react-redux and react-router to @travel-manager/functions

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -2,8 +2,8 @@ import jwt_decode from 'jwt-decode';
 
 export { createSlice, configureStore } from "@reduxjs/toolkit";
 export type { Action, ThunkAction } from "@reduxjs/toolkit";
-export { Provider as ReduxProvider } from "react-redux";
-export { BrowserRouter, Route, Routes, Navigate, Outlet } from "react-router-dom";
+export { Provider as ReduxProvider, useDispatch, useSelector } from "react-redux";
+export { BrowserRouter, Route, Routes, Navigate, Outlet, useParams, Link, useNavigate } from "react-router-dom";
 
 export const jwtDecode = jwt_decode;
   
