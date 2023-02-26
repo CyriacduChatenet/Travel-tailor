fe2d4401fd8deaaceda7f811ba326db12f10bd48
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-02-26 17:09:40 +0100

    {FIX}: update types lib

--- apps/api/src/activty/activty.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ActivtyService } from './activty.service';
-import { ActivtyController } from './activty.controller';
-
-@Module({
-  controllers: [ActivtyController],
-  providers: [ActivtyService]
-})
-export class ActivtyModule {}
--- apps/api/src/activty/dto/update-activty.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateActivtyDto } from './create-activty.dto';
-
-export class UpdateActivtyDto extends PartialType(CreateActivtyDto) {}
