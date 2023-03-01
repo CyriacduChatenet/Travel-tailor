33c48a71afae5f884eaf84be44d347f71040aeeb
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-01 19:50:45 +0100

    [FIX]: next config

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	experimental: {
+		transpilePackages: [
+			'@travel-tailor/types',
+			'@travel-tailor/functions',
+			'@travel-tailor/hooks',
+			'@travel-tailor/contexts',
+			'@travel-tailor/services',
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
