ac13f0c6d2c03023b495252bc20def3431cbf959
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-01 19:11:00 +0100

    [FIX]: update useFetcch hook

--- packages/hooks/src/fetch/index.ts
+++ packages/hooks/src/fetch/index.ts
@@ -13,7 +13,33 @@ export class UseFetchHook {
         return await response.json();
 	};
 
-	post = async (api_url: string, body?: any,  token?: AccessToken) => {
+	protectedGet = async (api_url: string, token: AccessToken) => {
+		const response = await fetch(api_url, {
+			headers: {
+				ContentType: 'application/json',
+				Accept: 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+			method: 'GET',
+		});
+
+        return await response.json();
+	};
+
+	post = async (api_url: string, body: Object) => {
+        const response = await fetch(api_url, {
+			headers: {
+				ContentType: 'application/json',
+				Accept: 'application/json'
+			},
+			method: 'POST',
+            body: JSON.stringify(body!)
+		});
+
+        return await response.json();
+    };
+
+	protectedPost = async (api_url: string, body: Object,  token: AccessToken) => {
         const response = await fetch(api_url, {
 			headers: {
 				ContentType: 'application/json',
@@ -27,7 +53,20 @@ export class UseFetchHook {
         return await response.json();
     };
 
-	patch = async (api_url: string, body: Object,  token?: AccessToken) => {
+	patch = async (api_url: string, body: Object) => {
+        const response = await fetch(api_url, {
+			headers: {
+				ContentType: 'application/json',
+				Accept: 'application/json',
+			},
+			method: 'PATCH',
+            body: JSON.stringify(body)
+		});
+
+        return await response.json();
+    };
+
+	protectedPatch = async (api_url: string, body: Object,  token: AccessToken) => {
         const response = await fetch(api_url, {
 			headers: {
 				ContentType: 'application/json',
@@ -41,7 +80,19 @@ export class UseFetchHook {
         return await response.json();
     };
 
-	delete = async (api_url: string, token: AccessToken,) => {
+	delete = async (api_url: string) => {
+        const response = await fetch(api_url, {
+			headers: {
+				ContentType: 'application/json',
+				Accept: 'application/json',
+			},
+			method: 'DELETE'
+		});
+
+        return await response.json();
+    };
+
+	protectedDelete = async (api_url: string, token: AccessToken) => {
         const response = await fetch(api_url, {
 			headers: {
 				ContentType: 'application/json',
