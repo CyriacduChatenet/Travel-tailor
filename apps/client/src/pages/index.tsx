d4f4935f31540320e0425c04de39a51a32da43af
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-02 12:29:16 +0100

    [FEAT]: clean home page

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,9 +1,27 @@
-import { NextPage } from "next";
+import { HelloService } from '@travel-tailor/services';
+import { NextPage } from 'next';
 
-const Home: NextPage = () => {
-  return (
-    <div>Hello next</div>
-  );
+import { Layout } from '@/layout';
+
+const Home: NextPage = ({ message }: any) => {
+	console.log(message);
+	return (
+		<Layout>
+			<>
+				<p>{message}</p>
+			</>
+		</Layout>
+	);
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+	const response = await HelloService.getHello();
+	console.log(response);
+	return {
+		props: {
+			message: response.message,
+		},
+	};
+};
