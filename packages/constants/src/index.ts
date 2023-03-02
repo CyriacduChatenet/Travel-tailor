b835353d8d0f665ea3c3ad55a5e2b1ff14669948
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-02 21:28:31 +0100

    [FEAT]: add constants

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -4,3 +4,8 @@ dotenv.config();
 
 export const API_SIGNIN_ROUTE = `${process.env.API_URL}/auth/signin`;
 export const API_SIGNUP_ROUTE = `${process.env.API_URL}/auth/signup`;
+export const API_FORGOT_PASSWORD_ROUTE = `${process.env.API_URL}/auth/forgot-password`;
+export const API_RESET_PASSWORD_ROUTE = `${process.env.API_URL}/auth/reset-password`;
+
+export const ACCESS_TOKEN = 'accessToken';
+export const RESET_TOKEN = 'resetToken';
