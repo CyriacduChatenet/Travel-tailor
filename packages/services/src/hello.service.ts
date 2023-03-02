317e47ccaf6e977ff23c438513c0ae570bb9c8cf
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-02 11:08:41 +0100

    [FEAT]: add hello service

--- packages/services/src/hello.service.ts
+++ packages/services/src/hello.service.ts
@@ -1,5 +1,12 @@
-const getHello = () => {};
+import { useFetch } from '@travel-tailor/hooks';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const getHello = () => {
+	return useFetch.get(`${process.env.API_URL}`);
+};
 
 export const HelloService = {
-    getHello,
-}
+	getHello,
+};
