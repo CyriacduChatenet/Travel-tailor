17bb09cdc06c4f5ff0a99f55fb44d84db13217f5
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-02 23:20:21 +0100

    [FEAT]: add useProtected hook

--- packages/hooks/src/routes/index.hook.ts
+++ packages/hooks/src/routes/index.hook.ts
@@ -1,10 +1,8 @@
 import { useEffect } from 'react';
-import { TokenService } from '@travel-tailor/services';
-import { ACCESS_TOKEN, ROUTES } from '@travel-tailor/constants';
+import { ROUTES } from '@travel-tailor/constants';
 
 export const useProtectedRoute = () => {
-	const token = TokenService.getToken(ACCESS_TOKEN);
-	const isAuth = token;
+	const isAuth = true;
     
 	useEffect(() => {
 		if (!isAuth) {
