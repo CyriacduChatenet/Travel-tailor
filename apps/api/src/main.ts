08300953d7c0d6f08e150b961ef7f56c1b944dff
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-03 21:09:17 +0100

    [FEAT] add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,27 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as path from 'path';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Travel tailor API')
+    .setDescription('The travel tailor API')
+    .setVersion('1.2')
+    .addTag('travel tailor')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/doc', app, document, {
+    customCssUrl: path.resolve(
+      __dirname,
+      '../node_modules/swagger-ui-dist/swagger-ui.css',
+    ),
+  });
+
   const corsOptions: CorsOptions = {
     origin: [`${process.env.CLIENT_APP_URL}`],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
