33cfe1df6f8755508dc285922cc73ddc7ac3c39f
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-03 14:47:53 +0100

    [FEAT] add services

--- packages/services/src/traveler/traveler.service.ts
+++ packages/services/src/traveler/traveler.service.ts
@@ -1,11 +1,16 @@
 import { API_TRAVELER_ROUTE } from "@travel-tailor/constants";
 import { useFetch } from "@travel-tailor/hooks";
-import { CreateTravelerDTO } from "@travel-tailor/types";
+import { CreateTravelerDTO, UpdateTravelerDTO } from "@travel-tailor/types";
 
 const createTraveler = (credentials: CreateTravelerDTO) => {
     return useFetch.post(API_TRAVELER_ROUTE, credentials)
 };
 
+const updateTraveler = (id: string, credentials: UpdateTravelerDTO, token: string) => {
+    return useFetch.protectedPatch(`${API_TRAVELER_ROUTE}/${id}`, credentials, token)
+};
+
 export const TravelerService = {
     createTraveler,
+    updateTraveler,
 };
