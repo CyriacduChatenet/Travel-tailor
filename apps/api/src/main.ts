a7843a8b32e363b5a40f1d3c8aaca3fd4f7ff886
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-03 21:20:00 +0100

    [FIX] files

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,6 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as path from 'path';
 
 import { AppModule } from './app.module';
 
@@ -15,12 +14,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/v1/doc', app, document, {
-    customCssUrl: path.resolve(
-      __dirname,
-      '../node_modules/swagger-ui-dist/swagger-ui.css',
-    ),
-  });
+  SwaggerModule.setup('api/v1/doc', app, document);
 
   const corsOptions: CorsOptions = {
     origin: [`${process.env.CLIENT_APP_URL}`],
