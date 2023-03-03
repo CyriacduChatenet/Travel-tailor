a0585e7853f24bf2c70153180185091c8c40f20b
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-03 10:23:47 +0100

    [FEAT]: add links between pages

--- apps/client/src/pages/forgot-password/index.tsx
+++ apps/client/src/pages/forgot-password/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 
 import { Layout } from "@/layout";
 import { WebForgotPasswordForm } from "@travel-tailor/ui";
+import Link from "next/link";
 
 const ForgotPasswordPage: NextPage = () => {
     return (
@@ -9,6 +10,7 @@ const ForgotPasswordPage: NextPage = () => {
             <h1>Forgot password</h1>
             <br />
             <WebForgotPasswordForm />
+            <Link href={'/signin'}>Signin</Link>
         </Layout>
     );
 };
