f39a922c4cf7b5233a0d98088d599109e039159b
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-03 14:49:53 +0100

    [FIX] fix files

--- packages/services/src/traveler/traveler.service.ts
+++ packages/services/src/traveler/traveler.service.ts
@@ -1,13 +1,14 @@
 import { API_TRAVELER_ROUTE } from "@travel-tailor/constants";
 import { useFetch } from "@travel-tailor/hooks";
 import { CreateTravelerDTO, UpdateTravelerDTO } from "@travel-tailor/types";
+import { TokenService } from "../tokens/token.service";
 
 const createTraveler = (credentials: CreateTravelerDTO) => {
     return useFetch.post(API_TRAVELER_ROUTE, credentials)
 };
 
-const updateTraveler = (id: string, credentials: UpdateTravelerDTO, token: string) => {
-    return useFetch.protectedPatch(`${API_TRAVELER_ROUTE}/${id}`, credentials, token)
+const updateTraveler = (id: string, credentials: UpdateTravelerDTO) => {
+    return useFetch.protectedPatch(`${API_TRAVELER_ROUTE}/${id}`, credentials, String(TokenService.getSigninToken()))
 };
 
 export const TravelerService = {
