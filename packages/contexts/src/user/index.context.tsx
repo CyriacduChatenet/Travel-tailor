5ba9819da44fc3462b2af076bccf228f6dcabb9c
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-04 19:12:03 +0100

    [FEAT] add user context

--- packages/contexts/src/user/index.context.tsx
+++ packages/contexts/src/user/index.context.tsx
@@ -1,11 +1,19 @@
-import React, { createContext, PropsWithChildren, useContext } from 'react';
+import React, { createContext, Dispatch, PropsWithChildren, SetStateAction, useContext, useState } from 'react';
 
-type Context = {};
+type Context = {
+	user: {};
+	setUser: Dispatch<SetStateAction<{}>>
+};
 
-const userContext = createContext<Context>({});
+const userContext = createContext<Context>({
+	user: {},
+	setUser: () => {},
+});
 
-export const UserContextProvider = ({ children}: PropsWithChildren) => {
-	return <userContext.Provider value={{}}>{children}</userContext.Provider>;
+export const UserContextProvider = ({ children }: PropsWithChildren) => {
+	const [user, setUser] = useState({});
+
+	return <userContext.Provider value={{user, setUser}}>{children}</userContext.Provider>;
 };
 
 export const useUser = () => useContext<Context>(userContext);
