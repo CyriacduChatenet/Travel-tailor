e22784fe05fbe60083692a14cc6bc81738232fbe
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-04 19:11:50 +0100

    [FEAT] add service

--- packages/services/src/user/user.service.ts
+++ packages/services/src/user/user.service.ts
@@ -2,10 +2,15 @@ import { useFetch } from "@travel-tailor/hooks";
 
 import { TokenService } from "../tokens/token.service";
 
+const findOneUser = (api_url: string) => {
+    return useFetch.get(`${api_url}`)
+};
+
 const updateUser = (api_url: string, body: any) => {
     return useFetch.protectedPatch(`${api_url}`, body, String(TokenService.getAccessToken()))
 };
 
 export const UserService = {
+    findOneUser,
     updateUser,
 };
