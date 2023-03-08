0be54761f9154a97aa456b89269b9408290dce39
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-08 11:28:14 +0100

    [FEAT] add services

--- packages/services/src/user/user.service.ts
+++ packages/services/src/user/user.service.ts
@@ -1,6 +1,9 @@
+import { ROLES } from '@travel-tailor/constants';
+import { jwtDecode } from '@travel-tailor/functions';
 import { useFetch } from '@travel-tailor/hooks'
 
-import { TokenService } from '../tokens/token.service'
+import { TokenService } from '../tokens/token.service';
+import { TravelerService } from '../traveler/traveler.service';
 
 const updateUser = (api_url: string, body: any) => {
   return useFetch.protectedPatch(
@@ -10,6 +13,23 @@ const updateUser = (api_url: string, body: any) => {
   )
 }
 
+const getUserByToken = async (api_url: string, email: string) => {
+  return await useFetch.get(`${api_url}/user/${email}`);
+};
+
+const getUserInfo = async (api_url: string) => {
+  const token = TokenService.getAccessToken();
+  const decodedToken = jwtDecode(String(token));
+  const user = await getUserByToken(api_url, decodedToken.email);
+
+  if(user.roles === ROLES.TRAVELER) {
+    const traveler = await TravelerService.getTravelerInfo(api_url, user.traveler.id);
+    return { ...user, ...traveler }
+  }
+};
+
 export const UserService = {
   updateUser,
+  getUserByToken,
+  getUserInfo,
 }
