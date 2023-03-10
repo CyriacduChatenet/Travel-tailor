29885cb37b9fc3f1e6c14ea2870035f27bf3d98f
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-10 18:28:47 +0100

    [FIX] api routes constants

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -1,15 +1,11 @@
-export const API_SIGNIN_ROUTE = `http://localhost:8000/api/v1/auth/signin`
-export const API_SIGNUP_ROUTE = `http://localhost:8000/api/v1/auth/signup`
-export const API_FORGOT_PASSWORD_ROUTE = `http://localhost:8000/api/v1/auth/forgot-password`
-export const API_RESET_PASSWORD_ROUTE = `http://localhost:8000/api/v1/auth/reset-password`
-
-export const API_ADVERTISER_ROUTE = `http://localhost:8000/api/v1/advertiser`
-
-export const API_TRAVELER_ROUTE = `http://localhost:8000/api/v1/traveler`
-
-export const API_TASTE_ROUTE = `http://localhost:8000/api/v1/taste`
-
-export const API_USER_ROUTE = `http://localhost:8000/api/v1/user`
+import { API_ADVERTISER_ROUTE } from "./api/auth"
+import { API_TRAVELER_ROUTE } from "./api/auth"
+import { API_TASTE_ROUTE } from "./api/auth"
+import { API_USER_ROUTE } from "./api/auth"
+import { API_SIGNIN_ROUTE } from "./api/auth"
+import { API_SIGNUP_ROUTE } from "./api/auth"
+import { API_FORGOT_PASSWORD_ROUTE } from "./api/auth"
+import { API_RESET_PASSWORD_ROUTE } from "./api/auth"
 
 export const ACCESS_TOKEN = 'accessToken'
 export const SIGNIN_TOKEN = 'signinToken'
