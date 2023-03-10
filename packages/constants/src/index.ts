ff50528b018401a7fd64784cba856bdebcf1260e
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-10 18:34:41 +0100

    [FIX] constants

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -1,35 +1,16 @@
-import { API_ADVERTISER_ROUTE } from "./api/auth"
-import { API_TRAVELER_ROUTE } from "./api/auth"
-import { API_TASTE_ROUTE } from "./api/auth"
-import { API_USER_ROUTE } from "./api/auth"
-import { API_SIGNIN_ROUTE } from "./api/auth"
-import { API_SIGNUP_ROUTE } from "./api/auth"
-import { API_FORGOT_PASSWORD_ROUTE } from "./api/auth"
-import { API_RESET_PASSWORD_ROUTE } from "./api/auth"
+export {
+  API_ADVERTISER_ROUTE,
+  API_TRAVELER_ROUTE,
+  API_TASTE_ROUTE,
+  API_USER_ROUTE,
+  API_SIGNIN_ROUTE,
+  API_SIGNUP_ROUTE,
+  API_FORGOT_PASSWORD_ROUTE,
+  API_RESET_PASSWORD_ROUTE,
+} from './api'
 
-export const ACCESS_TOKEN = 'accessToken'
-export const SIGNIN_TOKEN = 'signinToken'
-export const RESET_TOKEN = 'resetToken'
+export { ACCESS_TOKEN, SIGNIN_TOKEN, RESET_TOKEN } from './auth'
 
-export const ROUTES = {
-  SIGNIN: '/signin',
-  TRAVELER: {
-    DASHBOARD: '/traveler/dashboard',
-    TASTE: {
-      CREATE: '/traveler/create-taste',
-    },
-  },
-  ADVERTISER: {
-    DASHBOARD: '/advertiser/dashboard',
-    CREATE_ADVERTISER: '/advertiser/create-advertiser',
-  },
-  ADMIN: {
-    DASHBOARD: '/admin/dashboard',
-  },
-}
+export { ROUTES } from './client/routes'
 
-export const ROLES = {
-  ADMIN: 'admin',
-  TRAVELER: 'traveler',
-  ADVERTISER: 'advertiser',
-}
+export { ROLES } from './enum/roles';
